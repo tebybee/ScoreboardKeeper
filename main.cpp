6cd605842af86540c862acb9114340932719bb16
tebybee <tbybee@mail.csuchico.edu>
2018-04-12 11:30:51 -0700

    creates a scoreboard with the desired number of teams and scoring periods, and prints out that scoreboard using a function.

--- main.cpp
+++ main.cpp
@@ -7,12 +7,12 @@ const int MAX_PERIODS = 10;
 const int MAX_TEAMS = 4;
 const int MIN_PERIODS = 1;
 const int MIN_TEAMS = 1;
+int teams;
+int periods;
 vector<vector<int> > board;
+void printScoreboard(vector < vector<int> >);
 int main()
 {
-        int periods;
-        int teams;
-
         cout<<"How many competitors? ";
         cin>>teams;
         cout<<"How many scoring periods? ";
@@ -27,23 +27,33 @@ int main()
         }
         else
         {
-                cout<<"SCOREBOARD\n";
                 board.resize(teams);
                 for(int i=0; i< board.size(); i++)
                 {
                         board[i].resize(periods);
-                        cout<< "Player " <<i+1<< ": ";
                         for(int k=0; k< board[i].size(); k++)
                         {
                                 board[i][k] = 0;
-                                cout<<board[i][k]<< "|";
                         }
-                        cout<<"\n";
-                }        
+                } 
 
-                //make scoreboard and fill it with zeros
-
-                //once created, display the scoreboard
+                printScoreboard(board);
         }
         return 0;
 }
+void printScoreboard(vector< vector<int> > board)
+{
+        cout<<"SCOREBOARD\n";
+        //go through the score board and report the coresponding score
+        //based on the team and the scoring period.
+        for(int i=0; i< board.size(); i++)
+        {
+                cout<< "Player " <<i+1<< ": ";
+                for(int k=0; k< board[i].size(); k++)
+                {
+                        cout<<board[i][k]<< "|";
+                }
+                cout<<"\n";
+        } 
+}
+
